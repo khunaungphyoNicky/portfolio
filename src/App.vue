91265b388ee64740588c1e31db89e5d5b6295fad
aungphyo <khunaungphyo8800@gmail.com>
2023-12-07 13:05:20 +0630

    add hello world

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,7 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <div>
+    <h1>hello world</h1>
+  </div>
 </template>
 
 <style>
