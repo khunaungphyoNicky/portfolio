12ea296ed0c0e9ec1461ac24d473b9369674af6d
aungphyo <khunaungphyo8800@gmail.com>
2023-12-10 15:26:09 +0630

    activate menu bar

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,13 @@
 <template>
+  <div v-if="status" class="w-full h-screen bg-white flex flex-col gap-10 justify-center items-center z-20 fixed font-medium text-2xl duration-300 ease">
+      <a class="hover:text-[#147EFB] duration-200 ease-in-out" href="#home" @click="close()">Home</a>
+        <a class="hover:text-[#147EFB] duration-200 ease-in-out" href="#about" @click="close()">About</a>
+        <a class="hover:text-[#147EFB] duration-200 ease-in-out" href="#project" @click="close()">Project</a>
+        <a class="hover:text-[#147EFB] duration-200 ease-in-out" href="#contact" @click="close()">Contact</a>
+        <div class=" font-normal text-[29px] absolute top-6 right-12 close" @click="close()">
+        <i class="fa-solid fa-xmark"></i>
+      </div>
+    </div>
   <div class=" scroll-smooth bg-slate-100">
     <nav class=" bg-white shadow-md shadow-gray-200 items-center nav-bar py-6 px-12 text-black flex justify-between fixed top-0 w-full z-10">
       <div class="logo">
@@ -10,7 +19,7 @@
         <a class="hover:text-[#147EFB] duration-200 ease-in-out" href="#project">Project</a>
         <a class="hover:text-[#147EFB] duration-200 ease-in-out" href="#contact">Contact</a>
       </div>
-      <div class=" bar hidden font-bold text-[22px] opacity-80">
+      <div class=" bar hidden font-bold text-[22px] opacity-80" @click="menu()">
         <i class="fa-solid fa-bars"></i>
       </div>
     </nav>
@@ -32,6 +41,19 @@ import CopyRight from "./views/CopyRight.vue";
         name: 'App',
         components: {
           HomePage,AboutPage,ProjectPage,ContactPage,CopyRight
+        },
+        data() {
+          return {
+            status: false
+          }
+        },
+        methods: {
+          menu() {
+            this.status = true
+          },
+          close(){
+            this.status = false
+          }
         },
     }
 </script>
