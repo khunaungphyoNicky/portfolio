9e15cff58a257e49ee83d05f5d1de5ae9a29d0d9
Codin <139064963+khunaungphyoNicky@users.noreply.github.com>
2024-05-02 12:15:22 +0700

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,12 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import HomeView from '../App.vue'
 
 const routes: Array<RouteRecordRaw> = [
-  // {
-  //   path: '/',
-  //   name: 'home',
-  //   component: HomeView
-  // },
+   {
+     path: '/',
+     name: 'home',
+     component: HomeView
+   },
   
 ]
 
